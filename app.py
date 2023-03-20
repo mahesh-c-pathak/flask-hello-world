f254c6f876a75ff5e68c163b30526d75a2c67844
mahesh-c-pathak <93431683+mahesh-c-pathak@users.noreply.github.com>
2023-03-20 21:23:00 +0530

    Update app.py
    Checking if every push to your repo will automatically build your app and deploy it in production

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! Checking if every push to your repo will automatically build your app and deploy it in production'
